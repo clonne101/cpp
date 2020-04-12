232c0f3795897e46ee24b3c3f0f2c5710e85e089
clonne101 <jefferyosei101@gmail.com>
2020-04-12 12:03:27 +0000

    add in initial multi-dimensional arrays

---
+++ introduction/multi_dimensional_arrays/src/multi_dimensional_arrays.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : multi_dimensional_arrays.cpp
+// Author      : Jeffery Osei
+// Version     :
+// Copyright   : Open-Clonne
+// Description : Multi Dimensional Arrays in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int clonne[2][3] = {{2,3,4}, {8,9,10}};
+
+	cout << clonne[1][1] << endl;
+
+	return 0;
+}
