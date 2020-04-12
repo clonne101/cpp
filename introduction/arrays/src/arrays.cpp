1dfa7faff014f5a2c2c381f561bfab8cfbddfb58
clonne101 <jefferyosei101@gmail.com>
2020-04-12 11:19:00 +0000

    add in array initializer

---
+++ introduction/arrays/src/arrays.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : arrays.cpp
+// Author      : Jeffery Osei
+// Version     :
+// Copyright   : Open-Clonne
+// Description : Arrays in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int clonne[5] = {66,75,2,43,99};
+
+	cout << clonne[0] << endl;
+
+	return 0;
+}
