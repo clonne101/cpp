1700c19347dd88f10d82bf1ac9fa0632f42d0385
clonne101 <jefferyosei101@gmail.com>
2020-04-12 09:51:16 +0000

    add in do while loops

---
+++ introduction/do_while_loops/src/do_while_loops.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : do_while_loops.cpp
+// Author      : Jeffery Osei
+// Version     :
+// Copyright   : Open-Clonne
+// Description : Do While Loop in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x = 1;
+
+	do
+	{
+		cout << x << endl;
+		x++;
+	}
+	while(x < 10);
+
+	return 0;
+}
