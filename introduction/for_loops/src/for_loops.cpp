66aa0373b3df96f3fcca1cccf0052a1c01ffd8d2
clonne101 <jefferyosei101@gmail.com>
2020-04-12 09:22:31 +0000

    for loops

---
+++ introduction/for_loops/src/for_loops.cpp
@@ -0,0 +1,25 @@
+//============================================================================
+// Name        : for_loops.cpp
+// Author      : Jeffery Osei
+// Version     :
+// Copyright   : Open-Clonne
+// Description : For Loops in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for(int x = 0; x < 10; x++)
+	{
+		cout << x << endl;
+	}
+
+	for(int x = 5; x < 50; x+=5)
+	{
+		cout << x << endl;
+	}
+
+	return 0;
+}
