8968ddd014790825a132a9cc4167f19b294a5ad3
clonne101 <jefferyosei101@gmail.com>
2020-04-12 08:21:56 +0000

    Add in while loops

---
+++ introduction/while_loops/src/while_loops.cpp
@@ -0,0 +1,23 @@
+//============================================================================
+// Name        : while_loops.cpp
+// Author      : Jeffery Osei
+// Version     :
+// Copyright   : Open-Clonne
+// Description : While Loops in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int clonne = 0;
+
+	while(clonne <= 5)
+	{
+		cout << "clonne is: " << clonne << endl;
+		clonne = clonne + 1;
+	}
+
+	return 0;
+}
